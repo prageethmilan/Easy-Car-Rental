9f49d029f8a2c54df5e576b429e222e86ff6c169
prageethmilan <prageethmilan99@gmail.com>
2022-07-13 21:58:01 +0530

    Completed CarRentController

--- BackEnd/src/main/java/lk/ijse/spring/repo/CarRentRepo.java
+++ BackEnd/src/main/java/lk/ijse/spring/repo/CarRentRepo.java
@@ -2,11 +2,27 @@ package lk.ijse.spring.repo;
 
 import lk.ijse.spring.entity.CarRent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @author : M-Prageeth
  * @created : 07/07/2022 - 7:20 PM
  **/
 public interface CarRentRepo extends JpaRepository<CarRent, String> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE CarRent SET status=:status WHERE rentId=:rentId", nativeQuery = true)
+    void updateCarRentStatus(@Param("rentId") String rentId, @Param("status") String status);
+
+    List<CarRent> getAllByStatus(String status);
+
+    @Query(value = "SELECT cr from CarRent cr where cr.status=:status AND cr.driver.licenceNo=:licenceNo", nativeQuery = true)
+    List<CarRent> getAllByDrivingLicenceNo(@Param("status") String status, @Param("licenceNo") String licenceNo);
 
 }
