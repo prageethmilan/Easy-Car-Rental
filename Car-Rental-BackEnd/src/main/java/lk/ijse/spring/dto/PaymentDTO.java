293d5dde3fb8375a94fe28798d46614ada8ad71c
prageethmilan <prageethmilan99@gmail.com>
2022-07-18 20:20:23 +0530

    Change Module

---
+++ Car-Rental-BackEnd/src/main/java/lk/ijse/spring/dto/PaymentDTO.java
@@ -0,0 +1,29 @@
+package lk.ijse.spring.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+/**
+ * @author : M-Prageeth
+ * @created : 07/07/2022 - 7:06 PM
+ **/
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class PaymentDTO {
+    private String paymentId;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+    private String accountNo;
+    private String accountHolderName;
+    private String bankName;
+    private String branchName;
+    private double amount;
+    private CarRentDTO rental;
+}
---
+++ Car-Rental-BackEnd/src/main/java/lk/ijse/spring/service/CarRentService.java
@@ -0,0 +1,29 @@
+package lk.ijse.spring.service;
+
+import lk.ijse.spring.dto.CarRentDTO;
+
+import java.util.List;
+
+/**
+ * @author : M-Prageeth
+ * @created : 07/07/2022 - 7:23 PM
+ **/
+public interface CarRentService {
+    void addCarRent(CarRentDTO dto);
+
+    void updateCarRent(CarRentDTO dto);
+
+    void deleteCarRent(String rentId);
+
+    CarRentDTO searchCarRent(String rentId);
+
+    List<CarRentDTO> getAllCarRents();
+
+    String generateRentId();
+
+    void updateCarRentStatus(String rentId, String status);
+
+    List<CarRentDTO> getCarRentsByStatus(String status);
+
+    List<CarRentDTO> getCarRentsByDrivingLicenceNo(String status, String licenceNo);
+}
