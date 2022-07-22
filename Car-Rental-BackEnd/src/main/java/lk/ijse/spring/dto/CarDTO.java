baac45d1d262e480cb43861f128809c771c884a3
prageethmilan <prageethmilan99@gmail.com>
2022-07-22 21:52:12 +0530

    Complete Customer Dashboard

--- Car-Rental-BackEnd/src/main/java/lk/ijse/spring/dto/CarDTO.java
+++ Car-Rental-BackEnd/src/main/java/lk/ijse/spring/dto/CarDTO.java
@@ -21,10 +21,10 @@ public class CarDTO {
     private String transmissionType;
     private String fuelType;
     private String color;
-    private byte[] frontView;
-    private byte[] backView;
-    private byte[] sideView;
-    private byte[] internalView;
+    private String frontView;
+    private String backView;
+    private String sideView;
+    private String internalView;
     private double dailyRate;
     private double monthlyRate;
     private double freeKmForPrice;
